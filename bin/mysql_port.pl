74472c8126b44c5479424db09b802d5c525b4adc
arstercz <chenzhe07@gmail.com>
2018-06-28 12:41:15 +0800

    ignore the dup mysql port(such as enable extra_port)

--- bin/mysql_port.pl
+++ bin/mysql_port.pl
@@ -1,19 +1,29 @@
 #!/usr/bin/env perl
 # discovery port for zabbix mysql monitor
+use Data::Dumper;
 my $data = "{\"data\":[";
 open(PROCESS, "netstat -tunlp | grep mysqld |");
 my @item = <PROCESS>;
 my $i    = 0;
-foreach(@item) {
-    chomp;
-    if(/:(\d+)\s/){
-        $i++;
-        $data .= "\{\"{#MYSQLPORT}\":\"$1\"\}";
-        $data .= $i == $#item + 1
-               ? ']}'
-               : ',';
-    }
+my $j    = 0;
+my %h; 
+foreach (@item) {
+  chomp;
+  my @F = split(/\s+/, $_);
+  my($port) = ($F[3] =~ /(\d+$)/); 
+  my($proc) = ($F[-1] =~ /(\d+)/);
+  unless ($h{$proc} > 0 && $h{$proc} < $port) {
+    $h{$proc} = $port;
+    $i++;
+  }
+}
 
+foreach my $port (values %h) {
+  $j++;
+  $data .= "\{\"{#MYSQLPORT}\":\"$port\"\}";
+  $data .= $i == $j
+         ? ']}'
+         : ',';
 }
 print $data;
 close(PROCESS);
